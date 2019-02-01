67900cac2190f196590208b6936bde259ce13893
hhq <huiqi.he@solarsoft.org>
2019-02-01 15:37:27 +0800

    travis ci test

---
+++ HqTLVParseTests/HqTLVParseTests.m
@@ -0,0 +1,36 @@
+//
+//  HqTLVParseTests.m
+//  HqTLVParseTests
+//
+//  Created by hqmac on 2019/2/1.
+//  Copyright © 2019 macpro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HqTLVParseTests : XCTestCase
+
+@end
+
+@implementation HqTLVParseTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    NSLog(@"travis cli test");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
